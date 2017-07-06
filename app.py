b68eaede4590751257b06fd8fd21e27d74580f2a
Aarno Aukia <aarno.aukia@vshn.ch>
2017-07-06 13:37:35 +0300

    add prometheus metrics endpoint

--- app.py
+++ app.py
@@ -4,8 +4,14 @@ from flask import Flask
 app = Flask(__name__)
 from werkzeug.contrib.cache import SimpleCache
 cache = SimpleCache()
+from prometheus_client import Summary, Gauge, REGISTRY, generate_latest
+request_time = Summary('request_processing_seconds', 'Time spent processing http request')
+metrics_time = Summary('metrics_processing_seconds', 'Time spent processing metrics request')
+imap_time = Summary('imap_processing_seconds', 'Time spent processing imap request')
+flagged_items = Gauge('imap_flagged', 'Number of flagged items in IMAP mailbox')
 
 @app.route('/')
+@request_time.time()
 def index():
     items = cache.get('flagged-items')
     if items is None:
@@ -13,12 +19,22 @@ def index():
         cache.set('flagged-items', items, timeout=os.environ.get('cachetime',5*60))
     return items
 
+@imap_time.time()
 def get_flagged_items():
     imap=imaplib.IMAP4_SSL(os.environ.get('host'))
     imap.login(os.environ.get('username'), os.environ.get('password'))
     imap.select('"{0}"'.format(os.environ.get('folder','INBOX')), readonly=True)
     x,y=imap.search(None,'(FLAGGED)')
-    return str(len(y[0].split()))
+    flagged = len(y[0].split())
+    return str(flagged)
+
+@app.route('/metrics')
+@metrics_time.time()
+def metrics():
+    return generate_latest(REGISTRY)
+
+flagged_items.set_function(get_flagged_items)
 
 if __name__ == '__main__':
+    #print(os.environ)
     app.run(host='0.0.0.0',port=8080)
