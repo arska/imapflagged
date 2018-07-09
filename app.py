3998d402e995f71b7089e40a7634853fb5e526cc
Aarno Aukia <aarno.aukia@vshn.ch>
2018-07-09 13:23:30 +0300

    add tox (pylint) & travis, clean up code

--- app.py
+++ app.py
@@ -1,52 +1,124 @@
-import imaplib
+"""
+IMAP flagged items webservice
+"""
+
+import logging
 import os
 import time
-from flask import Flask, request
-app = Flask(__name__)
+import imaplib
 from werkzeug.contrib.cache import SimpleCache
-cache = SimpleCache()
-from prometheus_client import Histogram, Counter, Summary, Gauge, REGISTRY, generate_latest
-flagged_items = Gauge('imap_flagged', 'Number of flagged items in IMAP mailbox')
-imap_time = Summary('imap_processing_seconds', 'Time spent processing imap request')
-FLASK_REQUEST_LATENCY = Histogram('flask_request_latency_seconds', 'Flask Request Latency', ['method', 'endpoint'])
-FLASK_REQUEST_COUNT = Counter('flask_request_count', 'Flask Request Count', ['method', 'endpoint', 'http_status'])
+from flask import Flask, request
+from prometheus_client import Histogram, Counter, Gauge, Summary
+from prometheus_client import generate_latest, REGISTRY
+from dotenv import load_dotenv
 
-def before_request():
-    request.start_time = time.time()
+APP = Flask(__name__)  # Standard Flask app
+CACHE = SimpleCache()
 
-def after_request(response):
-    request_latency = max(time.time() - request.start_time, 0) # time can go backwards...
-    FLASK_REQUEST_LATENCY.labels(request.method, request.path).observe(request_latency)
-    FLASK_REQUEST_COUNT.labels(request.method, request.path, response.status_code).inc()
-    return response
+FLASK_REQUEST_LATENCY = Histogram(
+    'flask_request_latency_seconds',
+    'Flask Request Latency',
+    ['method', 'endpoint']
+)
 
-@app.route('/')
+FLASK_REQUEST_COUNT = Counter(
+    'flask_request_count',
+    'Flask Request Count',
+    ['method', 'endpoint', 'http_status']
+)
+
+FLASK_REQUEST_SIZE = Gauge(
+    'flask_request_size_bytes',
+    'Flask Response Size',
+    ['method', 'endpoint', 'http_status']
+)
+
+UPDATE_TIME = Summary(
+    'update_seconds',
+    'Time spent loading data upstream',
+    None
+)
+
+FLAGGED_ITEMS = Gauge(
+    'imap_flagged',
+    'number of flagged items in IMAP mailbox',
+    None
+)
+
+LOGFORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+logging.basicConfig(level=logging.DEBUG, format=LOGFORMAT)
+
+
+@APP.route('/metrics')
+def metrics():
+    """
+    Route returning metrics to prometheus
+    """
+    return generate_latest(REGISTRY)
+
+
+@APP.route('/')
 def index():
-    items = cache.get('flagged-items')
+    """
+    Return the cached number of items
+    """
+    items = CACHE.get('flagged-items')
     if items is None:
         items = get_flagged_items()
-        cache.set('flagged-items', items, timeout=os.environ.get('cachetime',5*60))
+        CACHE.set('flagged-items',
+                  items,
+                  timeout=os.environ.get('cachetime', 5*60))
     return items
 
-@imap_time.time()
+
+@UPDATE_TIME.time()
 def get_flagged_items():
-    imap=imaplib.IMAP4_SSL(os.environ.get('host'))
-    imap.login(os.environ.get('username'), os.environ.get('password'))
-    imap.select('"{0}"'.format(os.environ.get('folder','INBOX')), readonly=True)
-    x,y=imap.search(None,'(FLAGGED)')
+    """
+    Get the number of flagged items from the imap server
+    """
+    imap = imaplib.IMAP4_SSL(os.environ.get('host'))
+    imap.login(os.environ.get('username'),
+               os.environ.get('password'))
+    imap.select('"{0}"'.format(os.environ.get('folder', 'INBOX')),
+                readonly=True)
+    x, y = imap.search(None, '(FLAGGED)')  # noqa pylint: disable=invalid-name,unused-variable
     flagged = len(y[0].split())
     return str(flagged)
 
-@app.route('/metrics')
-def metrics():
-    return generate_latest(REGISTRY)
 
-# use the cached item count for the prometheus metric
-flagged_items.set_function(index)
+def before_request():
+    """
+    annotate the processing start time to each flask request
+    """
+    request.start_time = time.time()
 
-if __name__ == '__main__':
-    #print(os.environ)
-    app.before_request(before_request)
-    app.after_request(after_request)
-    app.run(host='0.0.0.0',port=8080)
 
+def after_request(response):
+    """
+    after returning the request calculate metrics about this request
+    """
+    # time can go backwards...
+    request_latency = max(time.time() - request.start_time, 0)
+    # pylint: disable-msg=no-member
+    FLASK_REQUEST_LATENCY.labels(request.method, request.path)\
+                         .observe(request_latency)
+    FLASK_REQUEST_SIZE.labels(request.method,
+                              request.path,
+                              response.status_code)\
+                      .set(len(response.data))
+    FLASK_REQUEST_COUNT.labels(request.method,
+                               request.path,
+                               response.status_code)\
+                       .inc()
+    return response
+
+
+if __name__ == "__main__":
+    # load settings from .env for development
+    load_dotenv()
+    # wire the prometheus metric to the index() function above
+    FLAGGED_ITEMS.set_function(index)
+
+    APP.before_request(before_request)
+    APP.after_request(after_request)
+    APP.run(host='0.0.0.0', port=os.environ.get('listenport', 8080))
